6e4e0301965889b2874b17f4b96b7f1e82afdb02
RuslanMudruj <13072003.rm@gmail.com>
2025-01-07 14:37:45 +0200

    solution6

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,7 +5,7 @@ export const USER_ID = 2154;
 const todosLink = '/todos';
 
 export const getTodos = () => {
-  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+  return client.get<Todo[]>(`${todosLink}?userId=${USER_ID}`);
 };
 
 export const addTodo = (todo: Omit<Todo, 'id' | 'userId' | 'completed'>) => {
@@ -17,11 +17,11 @@ export const addTodo = (todo: Omit<Todo, 'id' | 'userId' | 'completed'>) => {
 };
 
 export const deleteTodo = (todoId: number) => {
-  return client.delete(`${todosLink}${todoId}`);
+  return client.delete(`${todosLink}/${todoId}`);
 };
 
 export const updateTodo = (todo: Todo) => {
-  return client.patch<Todo>(`${todosLink}${todo.id}`, todo);
+  return client.patch<Todo>(`${todosLink}/${todo.id}`, todo);
 };
 
 // Add more methods here
